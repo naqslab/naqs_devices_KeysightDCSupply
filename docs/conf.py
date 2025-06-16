185da1cd4c6ebc5075cac3e3728cf0792e16ca78
Jason Pruitt <55477390+Json-To-String@users.noreply.github.com>
2025-06-16 17:38:53 -0400

    fix importlib metadata

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ project = 'naqs_devices_KeysightDCSupply'
 copyright = '2025, naqslab'
 author = 'naqslab'
 
-version = importlib_metadata.version('KeysightDCSupply')
+version = importlib.metadata.version('naqs_devices_KeysightDCSupply')
 release = version
 
 # get version into rst files
